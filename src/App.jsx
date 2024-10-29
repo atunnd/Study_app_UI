13e2efeadeadb62915028b2ac21e8232598784ff
atunnd <atuan.ngndai@gmail.com>
2024-10-29 23:11:09 +0700

    Ađing new home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Chatbot_page from './components/Chatbot_page'
+import React from 'react';
+import Login_page from './components/Login_page';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home_page from './components/home_page';
 
 const App = () => {
+  
   return (
-    <div>
-      <Chatbot_page />
-    </div>
+     <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={Home_page} />
+          <Route path="/login_page" Component={Login_page}/>
+        </Routes>
+     </BrowserRouter>
   )
 }
 
-export default App
+export default App;
