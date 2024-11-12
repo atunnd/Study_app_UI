6ad4c650f7b14640a9bdb7b9f6e8d05a83337c85
atunnd <atuan.ngndai@gmail.com>
2024-11-12 16:01:41 +0700

    Adding study page

--- src/components/Study_page.jsx
+++ src/components/Study_page.jsx
@@ -4,7 +4,10 @@ import './Study_page.css';
 const Study_page = () => {
   return (
     <div className="app-container">
-      <h1>Study Page</h1>
+      {/*Tasks board*/}
+      <label className='task-board'>
+        <h1 className='table-title'>Todo lists</h1>
+        </label>
     </div>
   );
 };
